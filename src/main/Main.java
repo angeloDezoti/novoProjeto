af3cfcc5dcd67ec255e95b71d6a7817a0b362e5a
a1711881 <a1711881@debian9-dacom.dacom>
2018-03-12 11:17:13 -0300

    Até aqui tá pronto PAAAAAAA confirma

---
+++ src/main/Main.java
@@ -0,0 +1,21 @@
+k/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author a1711881
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Fala galera tmj familiaS");
+    }
+    
+}
